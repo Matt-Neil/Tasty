f0a6d4bf287af1e44658cac750914116ba866dd4
Matt-Neil <neil.matt17@gmail.com>
2021-11-08 15:54:09 +0000

    dassad

--- frontend/tasty/src/API/image.js
+++ frontend/tasty/src/API/image.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: "http://tasty-env.eba-c5emmwpy.eu-west-2.elasticbeanstalk.com/api/image",
+    baseURL: "https://tasty-env.eba-c5emmwpy.eu-west-2.elasticbeanstalk.com/api/image",
     withCredentials: true
 });
