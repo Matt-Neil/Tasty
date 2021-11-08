ecbb93917f939c995647f8e02acad6a5589762b0
Matt-Neil <neil.matt17@gmail.com>
2021-11-08 16:10:12 +0000

    sda

--- frontend/tasty/src/API/recipes.js
+++ frontend/tasty/src/API/recipes.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: "https://tasty-env.eba-c5emmwpy.eu-west-2.elasticbeanstalk.com/api/recipes",
+    baseURL: "http://tasty-env.eba-c5emmwpy.eu-west-2.elasticbeanstalk.com/api/recipes",
     withCredentials: true
 });
